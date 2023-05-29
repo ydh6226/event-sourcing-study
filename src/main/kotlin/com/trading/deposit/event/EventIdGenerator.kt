cdf2f1fc8808738a42baae2bf8602235d2ec2d8a
ydh6226 <ydh6226@naver.com>
2023-05-29 18:18:20 +0900

    eventId 생성로직 변경

--- src/main/kotlin/com/trading/deposit/event/EventIdGenerator.kt
+++ src/main/kotlin/com/trading/deposit/event/EventIdGenerator.kt
@@ -1,11 +1,23 @@
 package com.trading.deposit.event
 
-import com.fasterxml.uuid.Generators
+import org.apache.commons.lang3.RandomStringUtils
+import java.time.Instant
 
 object EventIdGenerator {
 
     fun generate(): String {
-        val uuid = Generators.timeBasedGenerator().generate()!!
-        return uuid.toString()
+//        val uuid = Generators.timeBasedGenerator().generate()!!
+//        return uuid.toString()
+
+        val hexNanoSeconds = getCurrentHexNanoSeconds()
+        val randomAlphabetic = RandomStringUtils.randomAlphabetic(10)
+        return hexNanoSeconds + randomAlphabetic
+    }
+
+    private fun getCurrentHexNanoSeconds(): String {
+        val instant = Instant.now()
+        val milli = instant.toEpochMilli().toString()
+        val nano = String.format("%09d", instant.nano).substring(3)
+        return (milli + nano).toLong().toString(16)
     }
 }
